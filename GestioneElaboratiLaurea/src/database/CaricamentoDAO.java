19dea19a68282d0c5d6d3f6d2e9dd49a1908514e
marcofelix98 <marcocarlofeliciano@yahoo.it>
2020-02-07 16:59:41 +0100

    Verso l'implementazione di CaricamentoDAO e SedutaDiLaureaDAO
    Definiti i prototipi dei metodi di CaricamentoDAO e SedutaDiLaureaDAO,
    ho implementato i create per entrambi. Restano da fare read, update e
    delete.

--- GestioneElaboratiLaurea/src/database/CaricamentoDAO.java
+++ GestioneElaboratiLaurea/src/database/CaricamentoDAO.java
@@ -1,9 +1,73 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
+
+import entity.Assegnazione;
+import entity.Caricamento;
+import entity.SedutaDiLaurea;
+import enumeration.StatoElaborato;
+import enumeration.StatoStudente;
+
 public class CaricamentoDAO {
 
-	public CaricamentoDAO() {
-		// TODO Stub di costruttore generato automaticamente
+	public Caricamento create(Assegnazione assegnazione, SedutaDiLaurea seduta) throws DAOException {
+		Connection conn = null;
+		StudenteDAO studenteDAO = new StudenteDAO();
+		ElaboratoDAO elaboratoDAO = new ElaboratoDAO();
+		Caricamento caricamento = new Caricamento(assegnazione);
+		caricamento.setSedutaDiLaurea(seduta);
+		if(seduta.getScadenzaCaricamentoElaborati().before(java.sql.Date.valueOf(LocalDate.now()))) {
+			throw new DAOException("Impossibile caricare l'elaborato: data di scadenza superata");
+		}
+		try {
+			conn = DBManager.getConnection();
+			String query = "INSERT INTO CARICAMENTO (ASSEGNAZIONE, SEDUTADILAUREA, DATACARICAMENTO) "
+					+ "VALUES(?, ?, CURRENT_TIMESTAMP);";
+			PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, caricamento.getIdAssegnazione());
+			stmt.setInt(2, caricamento.getSedutaDiLaurea().getIdSedutaDiLaurea());
+			conn.setAutoCommit(false);
+			// begin transaction
+			stmt.executeUpdate();
+			elaboratoDAO.update(StatoElaborato.CARICATO_STUD, caricamento); // caricamento eredita da elaborato
+			studenteDAO.update(StatoStudente.PRONTO, caricamento.getStudente());
+			// end transaction
+			conn.commit();
+			ResultSet result = stmt.getGeneratedKeys();
+			if(result.next()) {
+				caricamento.setIdCaricamento(result.getInt("IDCARICAMENTO"));
+			}
+			caricamento.setDataAssegnazione(java.sql.Date.valueOf(LocalDate.now()));
+			return caricamento;
+		} catch (SQLException e) {
+			throw new DAOException("Caricamento non riuscito");
+		} finally {
+			try {
+				conn.setAutoCommit(true);
+			} catch (SQLException e) {
+				throw new DAOException("Problemi con la connessione");
+			}
+		}
+	}
+	
+	public Caricamento read(Caricamento caricamento) {
+		return caricamento;
+	}
+	
+	public List<Caricamento> read(SedutaDiLaurea seduta) {
+		return null;
 	}
 
+	public void update() {}
+	
+	public boolean delete(Caricamento caricamento) {
+		return false;
+	}
+	
 }
--- GestioneElaboratiLaurea/src/database/SedutaDiLaureaDAO.java
+++ GestioneElaboratiLaurea/src/database/SedutaDiLaureaDAO.java
@@ -1,9 +1,45 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import entity.SedutaDiLaurea;
+
 public class SedutaDiLaureaDAO {
 
-	public SedutaDiLaureaDAO() {
-		// TODO Stub di costruttore generato automaticamente
+	public SedutaDiLaurea create(Date scadenzaCaricamentoElaborati) throws DAOException {
+		SedutaDiLaurea seduta = new SedutaDiLaurea();
+		seduta.setScadenzaCaricamentoElaborati(scadenzaCaricamentoElaborati);
+		try {
+			Connection conn = DBManager.getConnection();
+			String query = "INSERT INTO SEDUTADILAUREA (SCADENZACARICAMENTOELABORATI) VALUES(?);";
+			PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stmt.setDate(1, (java.sql.Date) scadenzaCaricamentoElaborati);
+			stmt.executeUpdate();
+			ResultSet result = stmt.getGeneratedKeys();
+			if(result.next()) {
+				seduta.setIdSedutaDiLaurea(result.getInt("IDSEDUTADILAUREA"));
+			}
+			return seduta;
+		} catch (SQLException e) {
+			throw new DAOException("Non è stato possibile creare la seduta");
+		}
 	}
 
+	public SedutaDiLaurea read(SedutaDiLaurea seduta) {
+		return seduta;
+	}
+	
+	public SedutaDiLaurea update(SedutaDiLaurea seduta, Date nuovaData) {
+		seduta.setScadenzaCaricamentoElaborati(nuovaData);
+		return seduta;
+	}
+	
+	public boolean delete(SedutaDiLaurea seduta) {
+		return false;
+	}
 }
